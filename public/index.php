179a793856a50ef18edeff977010aa18e634c5f9
Katerina Denisova <catrinblaidd@yandex.ru>
2021-09-04 14:12:02 +0300

    feat: index.php shows update and check dates

--- public/index.php
+++ public/index.php
@@ -7,10 +7,20 @@ mb_internal_encoding("UTF-8");
 define('SP', DIRECTORY_SEPARATOR);
 $root = $_SERVER['DOCUMENT_ROOT'];
 if (SP == '\\') $root .= '/';
-define('ROOT', \str_replace('/', SP, $root));
+define('ROOT', str_replace('/', SP, $root));
 define('PUBL', __DIR__ . SP);
-define('HTML', \str_replace([ROOT, SP], ['', '/'], PUBL));
-define('APP', \realpath('..' . SP) . SP);
+define('HTML', str_replace([ROOT, SP], ['', '/'], PUBL));
+define('APP', realpath('..' . SP) . SP);
+define('DATA', APP . 'data' . SP);
 define('LIB', APP . 'lib' . SP);
 
-echo 'Hi';
+$updated = date('d.m.Y H:i:s', file_get_contents(DATA . 'lastUpdated'));
+$checked = file_get_contents(DATA . 'lastChecked');
+
+$html = "
+    <p>Последнее обновление: {$updated}</p>
+    <p>Последняя проверка: {$checked}</p>
+";
+
+header('Content-Type: text/html; charset=utf-8');
+echo $html;
