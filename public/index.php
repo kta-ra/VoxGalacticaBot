aac4f79adae5c316cdb5eca2e5136b33d920b40c
Katerina Denisova <catrinblaidd@yandex.ru>
2021-09-04 14:57:22 +0300

    feat: update button added

--- public/index.php
+++ public/index.php
@@ -12,15 +12,22 @@ define('PUBL', __DIR__ . SP);
 define('HTML', str_replace([ROOT, SP], ['', '/'], PUBL));
 define('APP', realpath('..' . SP) . SP);
 define('DATA', APP . 'data' . SP);
-define('LIB', APP . 'lib' . SP);
+define('SRC', APP . 'src' . SP);
+
+if (isset($_POST['update'])) {
+    require_once(SRC . 'news.php');
+}
 
 $updated = date('d.m.Y H:i:s', file_get_contents(DATA . 'lastUpdated'));
 $checked = file_get_contents(DATA . 'lastChecked');
 
-$html = "
-    <p>Последнее обновление: {$updated}</p>
-    <p>Последняя проверка: {$checked}</p>
-";
+$html = '
+    <p>Последнее обновление: '.$updated.'</p>
+    <p>Последняя проверка: '.$checked.'</p>
+    <form method="post">
+        <input type="submit" name="update" value="Проверить обновление">
+    </form>
+';
 
 header('Content-Type: text/html; charset=utf-8');
 echo $html;
