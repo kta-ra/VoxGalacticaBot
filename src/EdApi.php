2ab244480e0f6b8684e0a0893e58f0f693423197
Katerina Denisova <catrinblaidd@yandex.ru>
2021-09-05 15:05:09 +0300

    fix: base url fixed

--- src/EdApi.php
+++ src/EdApi.php
@@ -4,7 +4,7 @@ class EdApi extends Api {
     /**
      * @var string Базовый путь api
      */
-    protected $baseUrl = 'https://cms.zaonce.net/ru-RU/jsonapi/node/galnet_article';
+    protected $baseUrl = 'https://cms.zaonce.net/ru-RU/jsonapi/node/galnet_article/';
     
     /**
      * @var array Данные запроса
@@ -27,7 +27,10 @@ class EdApi extends Api {
         if (!is_null($limit)) $this->requestData['page']['limit'] = $limit;
         $url = $this->buildRequestUrl();
         $resultData = $this->curl->sendGetRequest($url);
-        if ($resultData['success']) $resultData['result'] = json_decode($resultData['result']);
+        if ($resultData['success']) {
+            $resultData['result'] = json_decode($resultData['result']);
+            $resultData['result']['data'] = array_reverse($resultData['result']['data']); //статьи от старых к новым
+        }
         return $resultData;
     }
     
